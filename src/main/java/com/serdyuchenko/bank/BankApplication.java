8c5f0ba9b93d97fffb5822114a710282a8a921c0
Anton Serdyuchenko <anton415@gmail.com>
2025-11-09 21:41:24 +0300

     Create the Application Entry Point

---
+++ src/main/java/com/serdyuchenko/bank/BankApplication.java
@@ -0,0 +1,15 @@
+package com.serdyuchenko.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Anton Serdyuchenko
+ */
+@SpringBootApplication
+public class BankApplication {
+    public static void main(String[] args) { 
+        SpringApplication.run(BankApplication.class, args); 
+    }
+}
